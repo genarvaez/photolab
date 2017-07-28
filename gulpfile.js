04476874db48386c9b8d81bcf81bb8c4041afc53
genarvaez <ge_naravez@hotmail.com>
2017-07-28 16:35:39 -0400

    navbar, seccionUno y seccionDos listas

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require("gulp");
+var concat = require ('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require ('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var webServer = require('gulp-webserver');
+
+gulp.task('script', function(){
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('dist/js/'))
+})
+
+gulp.task('style', function(){
+	gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'assets/sass/main.scss'])
+	.pipe(sass().on('error', sass.logError))
+	.pipe(minifyCss())
+	.pipe(concat('style.min.css'))
+	.pipe(gulp.dest('dist/css/'));
+
+})
+
+gulp.task('webServer', function(){
+	gulp.src('../photolab/')
+		.pipe(webServer({
+			fallback: 'index.html',
+			livereload: true,
+			directorylisting: false,
+			open: true
+		}))
+})
+
+
+gulp.task('watch', function(){
+	gulp.watch('assets/sass/*.scss', ['style']);
+})
+
+gulp.task('default',['script', 'style', 'webServer', 'watch'])
